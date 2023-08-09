56773f0f58b9418b2864a1bc5558716fdd17cd64
priyankambliya <priyankambliya18@gmail.com>
2023-08-09 15:00:47 +0530

    remove part of unnacessary comment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-// This is a database connection method 
+// database connection method 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => {
         console.log('databse connection established...')
